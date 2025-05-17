e45c4e24ed0ce7fcb791d0ebdbf660b588e852a7
Jose6348 <jfalasco88@gmail.com>
2025-05-16 21:54:54 -0300

    layout app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Slot, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+function RootLayoutNav() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login');
+  }, []);
+
+  return <Slot />;
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,10 +30,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <RootLayoutNav />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
